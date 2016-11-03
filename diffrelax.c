221ed328d25b6de4ae60929b475f18e7d3ddf5c3
Adei Josol <helena.josol@gmail.com>
2016-11-03 21:23:15 +0000

    Refer to array dimension as data_dim

--- diffrelax.c
+++ diffrelax.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     /*
         Parameters:
         1. Name of file containing space-separated square array of doubles (char *)
-        2. Array dimension (int)
+        2. Data array dimension (int)
         3. Number of threads (int)
         4. Precision (double), 1e-6 preferred
         5. DEBUG (char *), option to enable debugging statements
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     */
 
     double **data_array;
-    int dimension;
+    int data_dim;
     int num_threads;
     double precision;
     FILE *data_file;
@@ -40,30 +40,30 @@ int main(int argc, char *argv[])
 
     if (debug) {
         printf("debug: data_file(char *)=%s\n", argv[1]);
-        printf("debug: dimension(char *)=%s\n", argv[2]);
+        printf("debug: data_dim(char *)=%s\n", argv[2]);
         printf("debug: num_threads(char *)=%s\n", argv[3]);
         printf("debug: precision(char *)=%s\n", argv[4]);
     }
 
     /* Convert arguments to appropriate types */
-    dimension = atoi(argv[2]);
+    data_dim = atoi(argv[2]);
     num_threads = atoi(argv[3]);
     precision = atof(argv[4]);
 
     if (debug) {
-        printf("debug: dimension(int)=%d\n", dimension);
+        printf("debug: data_dim(int)=%d\n", data_dim);
         printf("debug: num_threads(int)=%d\n", num_threads);
         printf("debug: precision(double)=%.8f\n", precision);
     }
 
     /* Allocate memory for 2D array */
-    data_array = (double **) malloc(dimension * sizeof(double *));
+    data_array = (double **) malloc(data_dim * sizeof(double *));
     if (data_array == NULL) {
         printf("error: could not allocate memory for 2D array, aborting ...\n");
         return 1;
     }
-    for (i = 0; i < dimension; i++) {
-        data_array[i] = (double *) malloc(dimension * sizeof(double));
+    for (i = 0; i < data_dim; i++) {
+        data_array[i] = (double *) malloc(data_dim * sizeof(double));
         if (data_array[i] == NULL) {
             printf("error: could not allocate memory for row %d of 2D array, aborting ...\n", i);
             return 1;
@@ -78,12 +78,12 @@ int main(int argc, char *argv[])
     }
 
     /* Store data in 2D array */
-    load_data_to_array(data_array, dimension, data_file);
+    load_data_to_array(data_array, data_dim, data_file);
 
     if (debug) {
         printf("debug(load_data_to_array):\n");
-        for (i = 0; i < dimension; i++) {
-            for (j = 0; j < dimension; j++) {
+        for (i = 0; i < data_dim; i++) {
+            for (j = 0; j < data_dim; j++) {
                 printf("%.8f ", data_array[i][j]);
             }
             putchar('\n');
@@ -100,8 +100,8 @@ int main(int argc, char *argv[])
     }
 
     /* Average the four neighbours of non-boundary numbers */
-    for (i = 1; i < dimension - 1; i++) {
-        for (j = 1; j < dimension - 1; j++) {
+    for (i = 1; i < data_dim - 1; i++) {
+        for (j = 1; j < data_dim - 1; j++) {
             avg_array[i - 1][j - 1] = (data_array[i - 1][j] + data_array[i][j - 1]
                     + data_array[i][j + 1] + data_array[i + 1][j]) / 4.0f;
         }
@@ -122,7 +122,7 @@ int main(int argc, char *argv[])
         free(avg_array[i]);
     }
     free(avg_array);
-    for (i = 0; i < dimension; i++) {
+    for (i = 0; i < data_dim; i++) {
         free(data_array[i]);
     }
     free(data_array);
@@ -130,11 +130,11 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void load_data_to_array(double **data_array, int dimension, FILE *data_file)
+void load_data_to_array(double **data_array, int data_dim, FILE *data_file)
 {
     int i, j;
-    for (i = 0; i < dimension; i++) {
-        for (j = 0; j < dimension; j++) {
+    for (i = 0; i < data_dim; i++) {
+        for (j = 0; j < data_dim; j++) {
             fscanf(data_file, "%lf", &data_array[i][j]);
         }
         fgetc(data_file);
