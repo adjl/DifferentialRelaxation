c8f5f6b4fcc87a2e78b4ccfd373ecaa84bea5355
Adei Josol <helena.josol@gmail.com>
2016-11-04 19:57:22 +0000

    Read gen_rand_array script input from command line

--- gen_rand_array.py
+++ gen_rand_array.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
+# Example: $ ./gen_rand_array.py 4
+
 from random import randint, random
+from sys import argv
 
 def gen_rand_array(n):
     for i in xrange(n * n):
         print random() * randint(1, 100),
         if i % n == n - 1:
             print
+
+if __name__ == '__main__':
+    gen_rand_array(int(argv[1]))
