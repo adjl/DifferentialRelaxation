26c573aa38eed6f97281440d1be327e089cb70f2
Adei Josol <helena.josol@gmail.com>
2016-11-21 06:11:41 +0000

    Comment some parts of the code

--- diffrelax_prl.c
+++ diffrelax_prl.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 
 #define NUM_PARAMS 5
-#define DISP_WIDTH 12
-#define DISP_PRECN 8
+#define DISP_WIDTH 12 /* Modify this to change the output width of the values */
+#define DISP_PRECN 8  /* Likewise for the display precision */
 
 typedef struct {
     pthread_barrier_t *resume_barrier, *pause_barrier;
@@ -41,10 +41,16 @@ int main(int argc, char *argv[])
     int data_dim, num_threads, num_cells;
     int all_precise, num_precise, run, err, i;
 
+    /* Initialise variables ------------------------------------------------- */
+
+    all_precise = 0;
+    num_precise = 0;
+    run = 0;
+
     /* Read arguments ------------------------------------------------------- */
 
-    if (argc != NUM_PARAMS) {
-        printf("error: incorrect number of parameters, aborting ...\n");
+    if (argc < NUM_PARAMS) {
+        printf("error: too few arguments, aborting ...\n");
         return 1;
     }
 
@@ -57,7 +63,22 @@ int main(int argc, char *argv[])
     printf("num_threads=%d\n", num_threads);
     printf("precision=%*.*f\n\n", DISP_WIDTH, DISP_PRECN, precision);
 
-    /* Initialise pthread_* ------------------------------------------------- */
+    /* Load values into data array ------------------------------------------ */
+
+    data_array = malloc_array(data_dim);
+    if (data_array == NULL) {
+        printf("error: could not allocate memory for data array, aborting ...\n");
+        return 1;
+    }
+    data_file = fopen(argv[1], "r");
+    if (data_file == NULL) {
+        printf("error: could not open data file, aborting ...\n");
+        return 1;
+    }
+    load_data_values(data_file, data_array, data_dim);
+    fclose(data_file);
+
+    /* Initialise barriers and mutex ---------------------------------------- */
 
     err = pthread_barrier_init(&resume_barrier, NULL, num_threads + 1);
     if (err != 0) {
@@ -75,11 +96,7 @@ int main(int argc, char *argv[])
         return err;
     }
 
-    all_precise = 0;
-    num_precise = 0;
-    run = 0;
-
-    /* Initialise thread arguments array ------------------------------------ */
+    /* Initialise thread arguments ------------------------------------------ */
 
     thread_args = (pthread_args *) malloc(num_threads * sizeof(pthread_args));
     if (thread_args == NULL) {
@@ -102,40 +119,27 @@ int main(int argc, char *argv[])
 
     /* Split work amongst threads ------------------------------------------- */
 
-    num_cells = (data_dim - 2) * (data_dim - 2);
+    num_cells = (data_dim - 2) * (data_dim - 2); /* Number of cells to average */
     if (num_threads < num_cells) {
         int extra_load;
-        if (num_cells / 2 > num_cells - num_threads) {
+        if (num_cells / 2 > num_cells - num_threads) {  /* Extra cells <= half of total */
             extra_load = num_cells - num_threads;
         } else {
             extra_load = num_cells % num_threads;
         }
         for (i = num_threads - 1; i >= 0; i--) {
             thread_args[i].load += num_cells / num_threads;
-            if (extra_load-- > 0) thread_args[i].load++;
+            if (extra_load-- > 0) thread_args[i].load++; /* Spread extra load AMAP */
         }
     }
 
-    /* Load values into data array ------------------------------------------ */
-
-    data_array = malloc_array(data_dim);
-    if (data_array == NULL) {
-        printf("error: could not allocate memory for data array, aborting ...\n");
-        return 1;
-    }
-    data_file = fopen(argv[1], "r");
-    if (data_file == NULL) {
-        printf("error: could not open data file, aborting ...\n");
-        return 1;
-    }
-    load_data_values(data_file, data_array, data_dim);
-    fclose(data_file);
+    /* Create and initialise threads ---------------------------------------- */
 
     if (num_threads < num_cells) {
         int range_i;
         for (i = 0, range_i = 0; i < num_threads; range_i += thread_args[i].load, i++) {
             pthread_t thread;
-            thread_args[i].range_begin = range_i;
+            thread_args[i].range_begin = range_i; /* Work on range of cells */
             thread_args[i].range_end = range_i + thread_args[i].load;
             err = pthread_create(&thread, NULL,
                     (void * (*) (void *)) calculate_cell_avg,
@@ -148,14 +152,14 @@ int main(int argc, char *argv[])
     } else {
         for (i = 0; i < num_threads; i++) {
             pthread_t thread;
-            thread_args[i].range_begin = i;
+            thread_args[i].range_begin = i; /* Work on one cell */
             thread_args[i].range_end = i + 1;
             if (i < num_cells) {
                 err = pthread_create(&thread, NULL,
                         (void * (*) (void *)) calculate_cell_avg,
                         (void *) &thread_args[i]);
             } else {
-                err = pthread_create(&thread, NULL,
+                err = pthread_create(&thread, NULL, /* Extra (idle) thread */
                         (void * (*) (void *)) just_idle,
                         (void *) &thread_args[i]);
             }
@@ -166,6 +170,8 @@ int main(int argc, char *argv[])
         }
     }
 
+    /* Main thread loop ----------------------------------------------------- */
+
     for (;;) {
         double **avg_array = malloc_array(data_dim);
         if (avg_array == NULL) {
@@ -173,7 +179,7 @@ int main(int argc, char *argv[])
             return 1;
         }
 
-        for (i = 0; i < num_threads; i++) {
+        for (i = 0; i < num_threads; i++) { /* Update threads' array references */
             thread_args[i].data_array = data_array;
             thread_args[i].avg_array = avg_array;
         }
@@ -186,12 +192,12 @@ int main(int argc, char *argv[])
         printf("-----------------------------------------------------------\n");
 
         complete_and_print_array(data_array, avg_array, data_dim);
-        for (i = 0; i < data_dim; i++) {
+        for (i = 0; i < data_dim; i++) { /* Discard old values */
             free(data_array[i]);
         }
         free(data_array);
 
-        if (num_precise == num_cells) {
+        if (num_precise == num_cells) { /* All cells now within precision */
             all_precise = 1;
             pthread_barrier_wait(&resume_barrier);
             for (i = 0; i < data_dim; i++) {
@@ -201,9 +207,11 @@ int main(int argc, char *argv[])
             break;
         }
 
-        data_array = avg_array;
+        data_array = avg_array; /* Use averages array as next data array */
         num_precise = 0;
     }
+
+    /* Clean up after ourselves --------------------------------------------- */
 
     err = pthread_barrier_destroy(&resume_barrier);
     if (err != 0) {
@@ -220,7 +228,6 @@ int main(int argc, char *argv[])
         printf("error: could not destroy mutex, aborting ...\n");
         return err;
     }
-
     free(thread_args);
 
     return 0;
@@ -260,6 +267,10 @@ void calculate_cell_avg(pthread_args *args)
 
             i = k / args->avg_dim + 1;
             j = k % args->avg_dim + 1;
+
+            /** Calculate the average of the cell's four neighbours
+                Get the difference between the new and previous values
+                Increment counter if it is within the given precision */
 
             avg_array[i][j] = (data_array[i - 1][j] + data_array[i][j - 1]
                     + data_array[i][j + 1] + data_array[i + 1][j]) / 4.0f;
